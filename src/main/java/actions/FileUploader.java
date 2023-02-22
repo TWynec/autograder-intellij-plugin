64b9049edf4e6a3c78c624a74c9ba02d4becc508
Trace Wynecoop <trwyne03@gmail.com>
2023-02-22 12:42:04 -0800

    Read from file

--- src/main/java/actions/FileUploader.java
+++ src/main/java/actions/FileUploader.java
@@ -16,55 +16,48 @@ public class FileUploader {
         String urlString = "http://localhost/index.php";
 
         try {
-            // Open a connection to the server
             URL url = new URL(urlString);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-            //System.out.println(connection.getResponseCode());
 
             connection.setRequestMethod("POST");
             connection.setDoOutput(true);
 
-            // Set the request headers
             connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW");
 
-            // Open a stream to the file
             File file = new File(filePath);
             FileInputStream fileInputStream = new FileInputStream(file);
 
-            // Start building the request body
             DataOutputStream request = new DataOutputStream(connection.getOutputStream());
             request.writeBytes("------WebKitFormBoundary7MA4YWxkTrZu0gW\r\n");
             request.writeBytes("Content-Disposition: form-data; name=\"file\"; filename=\"" + file.getName() + "\"\r\n");
             request.writeBytes("Content-Type: application/octet-stream\r\n\r\n");
 
-            // Write the file
+            // Write
             int bytesRead;
             byte[] dataBuffer = new byte[1024];
             while ((bytesRead = fileInputStream.read(dataBuffer)) != -1) {
                 request.write(dataBuffer, 0, bytesRead);
             }
 
-            // Close the file input stream and request body
             fileInputStream.close();
             request.writeBytes("\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--");
             request.flush();
             request.close();
 
-            // Get the response
-            BufferedReader reader = null;
-            if (100 <= connection.getResponseCode() && connection.getResponseCode() <= 399) {
-                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                System.out.println(response.toString());
             } else {
-                reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+                System.out.println("POST request did not work.");
             }
-
-            //BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String response = reader.readLine();
-            reader.close();
-
-            // Print the response
-            System.out.println("Response from server: " + response);
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/actions/SubmitAction.java
+++ src/main/java/actions/SubmitAction.java
@@ -5,24 +5,18 @@ import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
-import javax.swing.JMenuBar;
-import javax.swing.JComponent;
-import java.nio.file.FileSystems;
-import java.nio.file.Paths;
-import java.nio.file.Path;
+
 
 public class SubmitAction extends AnAction {
-     @Override
+    @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
 
        VirtualFile virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE);
        String filePath = virtualFile.getPath();
-       //Path currentRelativePath = Paths.get("FileUploader.java");
-       //String s = filePath.toAbsolutePath().toString();
 
-         Messages.showInfoMessage(filePath,"Info");
-         FileUploader conn = new FileUploader();
+       Messages.showInfoMessage(filePath,"Info");
+       FileUploader conn = new FileUploader();
 
-         conn.uploadFile(filePath);
+       conn.uploadFile(filePath);
     }
 }
