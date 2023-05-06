2e85848021888f6b8d4ec20e4b35f10ac5b4fd06
theartur2000 <anesterov@ewu.edu>
2023-05-06 16:40:08 -0700

    Zip up directory

--- src/main/java/actions/FileUploader.java
+++ src/main/java/actions/FileUploader.java
@@ -9,12 +9,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.io.FileOutputStream;
+import java.util.zip.*;
 
 
 public class FileUploader {
 
     public void uploadFile(String filePath) {
-        String urlString = "http://localhost/";
+        String urlString = "http://home.bichael.net/uploads";
+        String urlString1 = "http://localhost/";
+
+
+
+        //TODO: add warning for Select Main Project Directory and submit that directory.
+
 
         try {
             URL url = new URL(urlString);
@@ -25,12 +33,34 @@ public class FileUploader {
 
             connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW");
 
-            File file = new File(filePath);
+            ///
+
+            ZipDirectory ZD = new ZipDirectory();
+            String sourceFile = filePath;
+
+            //System.out.println("ZIP PATH:" + getDir(filePath));
+            //System.out.println("ZIP PATH 2:" + filePath.substring(0,filePath.length()-4));
+
+            FileOutputStream fos = new FileOutputStream(getDir(filePath) + "\\dirCompressed.zip");
+            ZipOutputStream zipOut = new ZipOutputStream(fos);
+
+            File fileToZip = new File(sourceFile);
+            ZD.zipFile(fileToZip, fileToZip.getName(), zipOut);
+            zipOut.close();
+            fos.close();
+
+            String zipFilePath = (getDir(filePath) + "\\dirCompressed.zip"); //is the zipfile directory.
+
+
+            ///
+
+
+            File file = new File(zipFilePath);
             FileInputStream fileInputStream = new FileInputStream(file);
 
             DataOutputStream request = new DataOutputStream(connection.getOutputStream());
             request.writeBytes("------WebKitFormBoundary7MA4YWxkTrZu0gW\r\n");
-            request.writeBytes("Content-Disposition: form-data; name=\"file\"; filename=\"" + file.getName() + "\"\r\n");
+            request.writeBytes("Content-Disposition: form-data; name=\"files\"; filename=\"" + file.getName() + "\"\r\n");
             request.writeBytes("Content-Type: application/octet-stream\r\n\r\n");
 
             // Write
@@ -41,6 +71,8 @@ public class FileUploader {
             }
 
             fileInputStream.close();
+
+
             request.writeBytes("\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--");
             request.flush();
             request.close();
@@ -62,5 +94,14 @@ public class FileUploader {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String getDir(String filePath) {//go back one directory
+        int count = 0;
+        while (filePath.charAt(filePath.length()-1-count) != '/'){
+            count++;
+        }
+
+        return filePath.substring(0,filePath.length() - count - 1);
     }
 }
